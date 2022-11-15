58b7f75f3ea48f3acddb2e3437fb89e2ca2c70a6
ByeongHunKim <rkwhr912@naver.com>
2022-11-15 14:17:20 +0900

    :white_check_mark: confirmed : #3.3 Super User

---
+++ history/22_11_15/3_3_Super_User.py
@@ -0,0 +1,17 @@
+# admin 패널을 로깅하는 법
+
+"""
+    ~/p/Django_airbnb    main ?1  python manage.py createsuperuser
+
+Username (leave blank to use 'bstudent'):
+Email address:
+Password:
+Password (again):
+This password is too short. It must contain at least 8 characters.
+This password is too common.
+This password is entirely numeric.
+Bypass password validation and create user anyway? [y/N]: y
+Superuser created successfully.
+"""
+
+# 이렇게 되면 auth_user 테이블에 유저 정보 하나가 생성된다.
