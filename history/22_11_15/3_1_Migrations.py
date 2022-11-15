3878e28bc131f9ce57aab0a9dfb13f6d88fb4766
ByeongHunKim <rkwhr912@naver.com>
2022-11-15 13:44:56 +0900

    :white_check_mark: confirmed : #3.1 Migrations

---
+++ history/22_11_15/3_1_Migrations.py
@@ -0,0 +1,10 @@
+"""
+You have 18 unapplied migration(s). Your project may not work properly until you apply the migrations for app(s): admin, auth, contenttypes, sessions.
+Run 'python manage.py migrate' to apply them.
+"""
+
+# Django는 이미 DB state를 변경할 수 있는 파이썬 코드를 가지고 있다 위의 말은 우리 DB의 state를 변경시키는 파일이 18개 있다는 말
+
+# migration은 데이터베아스 state를 수정한다는 것 
+
+# python manage.py migrate
