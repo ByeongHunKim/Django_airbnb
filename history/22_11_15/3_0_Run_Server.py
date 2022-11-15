8d0fed54f101b3110b4e3c0e66272998c16a6a1f
ByeongHunKim <rkwhr912@naver.com>
2022-11-15 13:33:12 +0900

    :white_check_mark: confirmed : #3.0 Run Server

---
+++ history/22_11_15/3_0_Run_Server.py
@@ -0,0 +1,20 @@
+# manage.py는 Terminal에서 Django를 실행하기 위해 사용
+
+# Django를 실제 서버에 배포할 때는 서버를 실행할 때 manage.py 말고 다른걸 사용할 것.
+# 개발단계에서 VSCode로 작업할 때는 거의 모든 커맨드를 manage.py를 사용해 실행
+
+"""
+    ~/p/Django_airbnb    main ?1  python manage.py runserver       ✔  django-airbnb-w705VANB-py3.8   12:27:44 
+Watching for file changes with StatReloader
+Performing system checks...
+
+System check identified no issues (0 silenced).
+
+You have 18 unapplied migration(s). Your project may not work properly until you apply the migrations for app(s): admin, auth, contenttypes, sessions.
+Run 'python manage.py migrate' to apply them.
+November 15, 2022 - 03:27:49
+Django version 4.1.3, using settings 'config.settings'
+Starting development server at http://127.0.0.1:8000/
+Quit the server with CONTROL-C.
+
+"""
